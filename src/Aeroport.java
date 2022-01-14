fa643e3d6d5294a72ddbdb7a98c16e2447683ea0
bcrespin <crespin.baptiste08@gmail.com>
2022-01-14 14:54:50 +0100

    Test2

---
+++ src/Aeroport.java
@@ -0,0 +1,43 @@
+package src;
+public class Aeroport {
+
+    private String IATA;
+    private String Name;
+    private String country;
+    private double latitude;
+    private double longitude;
+
+
+    public Aeroport(String IATA, String NAME) {
+        this.IATA = IATA;
+        this.lastName = lastName;
+        totalNumberOfStudents++;
+    }
+
+
+    public String GetIATA() {
+        return IATA;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double GetLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public String toString() {
+
+    }
+
+
+    public static void main (String args[]){
+        System.out.println("Hello world");
+        }
+
+
+
+
+}
