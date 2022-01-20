690e54cd40d8cd83245b53f0e8ed048d6c03c2f7
bcrespin <crespin.baptiste08@gmail.com>
2022-01-20 17:31:37 +0100

    début IHM

--- src/Earth.java
+++ src/Earth.java
@@ -1,6 +1,14 @@
 package src;
 
 public class Earth {
+    public Earth() {
+        this.getChildren().add(sph);
+    }
 
 
+    public class Main {
+        public static void main(String args[]) {
+            Earth Terre = new Earth();
+        }
+    }
 }
