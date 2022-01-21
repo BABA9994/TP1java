f4efd5e3bae66f001d470dc61ec4e676c175c488
bcrespin <crespin.baptiste08@gmail.com>
2022-01-21 12:02:18 +0100

    Rotate Sphere

--- src/Earth.java
+++ src/Earth.java
@@ -1,14 +1,58 @@
 package src;
+import javafx.animation.AnimationTimer;
+import javafx.animation.RotateTransition;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Material;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
 
-public class Earth {
-    public Earth() {
+public class Earth extends Group {
+
+    // Ici on créer un cube au lieu d'une sphere
+    // Box sph = new Box();
+    Sphere sph = new Sphere();
+    Rotate rotate = new Rotate();
+    PhongMaterial mat = new PhongMaterial();
+
+
+    public Earth () {
+        sph.setRadius(300);
+        // sph.setWidth(300);
+        // sph.setHeight(300);
+        // sph.setDepth(300);
+
+        Image map = new Image("earth_lights_4800.png");
+        mat.setDiffuseMap(map);
+        sph.setMaterial(mat);
+
         this.getChildren().add(sph);
-    }
-//test 2
 
-    public class Main {
+        Rotate ry = new Rotate();
+
+        AnimationTimer animationTimer = new AnimationTimer() {
+            @Override
+            public void handle(long time) {
+                System.out.println("Valeur de time : " + time);
+                ry.setAngle(10); // A compl´eter
+                ry.setPivotX(0);
+                ry.setPivotY(0);
+                sph.getTransforms().add(ry);
+            }
+        };
+
+        animationTimer.start();
+
+    }
+
+
+
+
         public static void main(String args[]) {
-            Earth Terre = new Earth();
+
+            System.out.println("Hello Terre");
         }
     }
-}
+
--- src/Interface.java
+++ src/Interface.java
@@ -1,21 +1,61 @@
 package src;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.transform.Transform;
 import javafx.stage.Stage;
 
 public class Interface extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Hello world");
-        Group root = new Group();
-        Pane pane = new Pane(root);
-        Scene theScene = new Scene(pane, 1000, 700,true);
-        primaryStage.setScene(theScene);
+
+        // On créer la fenetre
+        primaryStage.setTitle("Hello la Terre");
+        Earth earth = new Earth();
+        Pane pane = new Pane(earth);
+        Scene ihm = new Scene(pane, 1000, 600,true);
+        primaryStage.setScene(ihm);
         primaryStage.show();
+
+        // On créer la caméra et faisons les réglages de la cam
+        PerspectiveCamera camera = new PerspectiveCamera(true);
+        camera.setTranslateZ(-2000);
+        camera.setNearClip(0.1);
+        camera.setFarClip(2000.0);
+        camera.setFieldOfView(35);
+        ihm.setCamera(camera);
+
+        final double[] Xclick = new double[1];
+        final double[] Yclick = new double[1];
+        final double[] Yrelease = new double[1];
+        final double[] Xrelease = new double[1];
+
+        // On créer les fonctionnalités de zoom
+        ihm.addEventHandler(MouseEvent.ANY, event -> {
+            if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
+                System.out.println("Clicked on : (" + event.getSceneX()+ ", "+ event.getSceneY()+")");
+                Xclick[0] = event.getSceneX();
+                Yclick[0] = event.getSceneY();
+            }
+            if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+                System.out.println("Clicked on DRAG : (" + event.getSceneX() + ", " + event.getSceneY() + ")");
+                Xrelease[0] = event.getSceneX();
+                Yrelease[0] = event.getSceneY();
+                double delta_X = Xclick[0] - Xrelease[0];
+                double delta_Y = Yclick[0] - Yrelease[0];
+
+
+                double ancien_z = camera.getTranslateZ();
+                double new_z = ancien_z + 0.05*delta_Y - 0.05*delta_X;
+                camera.setTranslateZ(new_z);
+                        //à vous de completer
+
+            }
+        });
     }
     public static void main(String[] args) {
         launch(args);
